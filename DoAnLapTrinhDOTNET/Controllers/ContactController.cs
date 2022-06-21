5250fa06c4e95b9f56dd1418f0b54a25714deb89
namkhangtb <khangnguyen201201@gmail.com>
2022-06-21 09:54:16 +0700

    up contact

---
+++ DoAnLapTrinhDOTNET/Controllers/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnLapTrinhDOTNET.Controllers
+{
+    public class ContactController : Controller
+    {
+        private LipstickDbContext db = new LipstickDbContext();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var lst = db.LienHes.ToList();
+            return View(lst);
+        }
+    }
+}
